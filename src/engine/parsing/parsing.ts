a0665a0326ef2f4161278cdd6a4ed2f355d9d93f
JTan2231 <j.tan2231@gmail.com>
2023-06-28 15:54:26 -0500

    init (unfinished)

---
+++ src/engine/parsing/parsing.ts
@@ -0,0 +1,37 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as c from './c';
+import * as graphs from '../graphs/function_graph';
+
+const SUPPORTED_FILETYPES: Record<string, (rootPath: string, verbose: number) => graphs.FunctionGraph> = {
+    'c': c.buildGraphs,
+};
+
+export function buildGraph(rootPath: string, verbose: number, language: string): graphs.FunctionGraph {
+    let buildFunction: ((rootPath: string, verbose: number) => graphs.FunctionGraph) | null = null;
+
+    if (language.length === 0) {
+      fs.readdirSync(rootPath).forEach((filename) => {
+        const fileType = path.extname(filename).substring(1);
+        console.log('fileType: ', fileType);
+        if (SUPPORTED_FILETYPES.hasOwnProperty(fileType)) {
+          buildFunction = SUPPORTED_FILETYPES[fileType];
+          return;
+        }
+      });
+    } else {
+      if (SUPPORTED_FILETYPES.hasOwnProperty(language)) {
+        buildFunction = SUPPORTED_FILETYPES[language];
+      } else {
+        throw new Error(`BuildGraph: language ${language} not supported`);
+      }
+    }
+
+    if (!buildFunction) {
+      throw new Error('No supported file types found');
+    }
+
+    const functionGraph = buildFunction(rootPath, verbose);
+
+    return functionGraph;
+}
